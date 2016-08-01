97f81d6afa6c4cae14cf43e8b3416c8a9f5f10b8
Rudolf Adamkovič <salutis@me.com>
2016-08-01 11:21:20 +0200

    Add UI tests for tab bar

--- Sources/EmotionCodeUITests/EmotionCodeUITests.swift
+++ Sources/EmotionCodeUITests/EmotionCodeUITests.swift
@@ -1,3 +1,50 @@
 import XCTest
 
-final class EmotionCodeUITests: XCTestCase {}
+// MARK: Main
+
+final class TabBarTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launch()
+        sleep(1)
+    }
+
+}
+
+// MARK: Tests
+
+extension TabBarTests {
+
+    func testAvailableTabs() {
+        let elements = app.tabBars.buttons.allElementsBoundByIndex
+        let labels = elements.map { $0.label }
+        XCTAssertEqual(labels, ["Book", "Chart", "Help"])
+    }
+
+    func testDefaultTab() {
+        XCTAssert(app.navigationBars["Book"].exists)
+        XCTAssert(app.buttons["Book"].selected)
+    }
+
+    func testSwitchingTabs() {
+        let labels = ["Chart", "Help", "Book"]
+        labels.forEach { label in
+            app.buttons[label].tap()
+            XCTAssert(app.buttons[label].selected)
+            XCTAssert(app.navigationBars[label].exists)
+        }
+    }
+
+}
+
+// MARK: App shortcut
+
+private extension TabBarTests {
+
+    var app: XCUIApplication {
+        return XCUIApplication()
+    }
+
+}
