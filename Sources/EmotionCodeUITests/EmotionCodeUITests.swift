aee6e584ed3c46441c6ffc98f604f1a33f5fdc4f
Rudolf Adamkovič <salutis@me.com>
2016-08-01 16:37:11 +0200

    Refactor tab bar tests

--- Sources/EmotionCodeUITests/EmotionCodeUITests.swift
+++ Sources/EmotionCodeUITests/EmotionCodeUITests.swift
@@ -13,28 +13,104 @@ final class TabBarTests: XCTestCase {
 
 }
 
-// MARK: Tests
+// MARK: Test layout
 
 extension TabBarTests {
 
-    func testAvailableTabs() {
-        let elements = app.tabBars.buttons.allElementsBoundByIndex
-        let labels = elements.map { $0.label }
-        XCTAssertEqual(labels, ["Book", "Chart", "Help"])
+    func testHasThreeTabs() {
+        XCTAssertEqual(app.tabBars.buttons.count, 3)
     }
 
-    func testDefaultTab() {
-        XCTAssert(app.navigationBars["Book"].exists)
+    func testHasBookTab() {
+        XCTAssertEqual(app.tabBars.buttons.elementBoundByIndex(0).label, "Book")
+    }
+
+    func testHasChartTab() {
+        XCTAssertEqual(app.tabBars.buttons.elementBoundByIndex(1).label, "Chart")
+    }
+
+    func testHasHelpTab() {
+        XCTAssertEqual(app.tabBars.buttons.elementBoundByIndex(2).label, "Help")
+    }
+
+}
+
+// MARK: Test defaults
+
+extension TabBarTests {
+
+    func testDefaultSelection() {
         XCTAssert(app.buttons["Book"].selected)
     }
 
-    func testSwitchingTabs() {
-        let labels = ["Chart", "Help", "Book"]
-        labels.forEach { label in
-            app.buttons[label].tap()
-            XCTAssert(app.buttons[label].selected)
-            XCTAssert(app.navigationBars[label].exists)
-        }
+    func testDefaultTitle() {
+        XCTAssert(app.navigationBars["Book"].exists)
+    }
+
+}
+
+// MARK: Test book selection
+
+extension TabBarTests {
+
+    func testBookSelection() {
+        app.buttons["Book"].tap()
+        XCTAssert(app.buttons["Book"].selected)
+    }
+
+    func testBookTitle() {
+        app.buttons["Book"].tap()
+        XCTAssert(app.navigationBars["Book"].exists)
+    }
+
+    func testBookDeselection() {
+        app.buttons["Book"].tap()
+        XCTAssertFalse(app.buttons["Chart"].selected)
+        XCTAssertFalse(app.buttons["Help"].selected)
+    }
+
+}
+
+// MARK: Test chart selection
+
+extension TabBarTests {
+
+    func testChartSelection() {
+        app.buttons["Chart"].tap()
+        XCTAssert(app.buttons["Chart"].selected)
+    }
+
+    func testChartTitle() {
+        app.buttons["Chart"].tap()
+        XCTAssert(app.navigationBars["Chart"].exists)
+    }
+
+    func testChartDeselection() {
+        app.buttons["Chart"].tap()
+        XCTAssertFalse(app.buttons["Book"].selected)
+        XCTAssertFalse(app.buttons["Help"].selected)
+    }
+
+}
+
+// MARK: Test help selection
+
+extension TabBarTests {
+
+    func testHelpSelection() {
+        app.buttons["Help"].tap()
+        XCTAssert(app.buttons["Help"].selected)
+    }
+
+    func testHelpTitle() {
+        app.buttons["Help"].tap()
+        XCTAssert(app.navigationBars["Help"].exists)
+    }
+
+    func testHelpDeselection() {
+        app.buttons["Help"].tap()
+        XCTAssertFalse(app.buttons["Book"].selected)
+        XCTAssertFalse(app.buttons["Chart"].selected)
     }
 
 }
