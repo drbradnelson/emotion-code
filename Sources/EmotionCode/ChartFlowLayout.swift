2011e9fc0220d717a27be31e2292551dab52b060
vbondarenko <vladimir@bondarenko3d.net>
2016-08-09 11:11:16 +0300

    Delete empty lines

--- Sources/EmotionCode/ChartFlowLayout.swift
+++ Sources/EmotionCode/ChartFlowLayout.swift
@@ -3,36 +3,36 @@ import UIKit
 // MARK: Main
 
 class ChartFlowLayout: UICollectionViewFlowLayout {
-    
+
     let itemHeight: CGFloat = 36
     let numberOfColumns: CGFloat = 2
-    
+
     override init() {
         super.init()
         setupLayout()
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupLayout()
     }
-    
+
 }
 
 // MARK: Layout
 
 extension ChartFlowLayout {
-    
+
     func setupLayout() {
         minimumInteritemSpacing = 1
         minimumLineSpacing = 5
         scrollDirection = .Vertical
     }
-    
+
     func itemWidth() -> CGFloat {
         return ((collectionView!.frame).width / numberOfColumns) - 1
     }
-    
+
     override var itemSize: CGSize {
         set {
             self.itemSize = CGSize(width: itemWidth(), height: itemHeight)
@@ -41,5 +41,5 @@ extension ChartFlowLayout {
             return CGSize(width: itemWidth(), height: itemHeight)
         }
     }
-    
+
 }
