1012c36085d2aafa8650f3f7d98dc48c7635293e
Russ Kamalov <ruslankmlv@gmail.com>
2016-08-12 08:12:44 -0700

    Update html validation script

--- Sources/EmotionCode/MarkupLint.sh
+++ Sources/EmotionCode/MarkupLint.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash -e
 
-tidy --help | grep 'This is modern HTML Tidy' || { echo "Tidy needs to be updated to support HTML5"; exit 1; }
+if ! tidy -h | grep -q 'This is modern HTML Tidy'
+then
+echo "Tidy needs to be updated to support HTML5" >&2
+exit 1
+fi
+
 echo "Looking for invalid markup files:"
 find * -iname '*.html' -type f | while read FILE
 do
-    echo "Linting: $FILE"
-    tidy -q -e "$FILE"
+echo "Linting: $FILE"
+tidy -q -e "$FILE"
 done
