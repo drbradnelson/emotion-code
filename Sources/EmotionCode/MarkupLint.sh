df06a7a3c633bdf7c7c878101e5cd5044d615373
Russ Kamalov <ruslankmlv@gmail.com>
2016-08-15 09:48:26 -0700

    Format MarkupLint indentation

--- Sources/EmotionCode/MarkupLint.sh
+++ Sources/EmotionCode/MarkupLint.sh
@@ -2,13 +2,13 @@
 
 if ! tidy -h | grep -q 'This is modern HTML Tidy'
 then
-echo "Tidy needs to be updated to support HTML5" >&2
-exit 1
+    echo "Tidy needs to be updated to support HTML5" >&2
+    exit 1
 fi
 
 echo "Looking for invalid markup files:"
 find * -iname '*.html' -type f | while read FILE
 do
-echo "Linting: $FILE"
-tidy -q -e "$FILE"
+    echo "Linting: $FILE"
+    tidy -q -e "$FILE"
 done
