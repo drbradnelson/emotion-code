bb4ff5bcebd356b8510a08c627b70a93f361487d
Russ Kamalov <ruslankmlv@gmail.com>
2016-08-16 11:48:10 -0700

    Load chapter HTML file

--- Sources/EmotionCode/BookChapterViewController.swift
+++ Sources/EmotionCode/BookChapterViewController.swift
@@ -2,4 +2,35 @@ import UIKit
 
 final class BookChapterViewController: UIViewController {
 
+    @IBOutlet private(set) var webView: UIWebView!
+    var chapterIndex = 0
+    var chapterURL: NSURL?
+
+}
+
+// MARK: View lifecycle
+
+extension BookChapterViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadChapter()
+    }
+
+}
+
+// MARK: Load chapter html
+
+private extension BookChapterViewController {
+
+    func loadChapter() {
+        guard let chapterURL = chapterURL else { return }
+        do {
+            let htmlString = try String(contentsOfURL: chapterURL)
+            webView.loadHTMLString(htmlString, baseURL: chapterURL)
+        } catch {
+            print(error)
+        }
+    }
+
 }
