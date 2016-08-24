6ef24e2312e61f5644df3bbcff096d4494139723
Russ Kamalov <ruslankmlv@gmail.com>
2016-08-24 10:19:32 -0700

    Add BookChapterSelectionLayoutTests

---
+++ Sources/EmotionCodeUITests/BookChapterSelectionLayoutTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+
+// MARK: Main
+
+final class BookChapterSelectionLayoutTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launch()
+        sleep(1)
+        app.buttons["Chapter 1"].tap()
+    }
+
+}
+
+// MARK: Tests
+
+extension BookChapterSelectionLayoutTests {
+
+    func testCancelButton() {
+        XCTAssert(app.navigationBars["Table of Contents"].buttons["Cancel"].hittable)
+    }
+
+}
