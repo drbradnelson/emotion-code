f32a203e732ee51176d0fd95b048fc959c31154b
Andre Lami <andre.lami.work@gmail.com>
2016-09-01 10:46:18 +0300

    Add accessibility to chart overview controller

---
+++ Sources/EmotionCode/ChartOverviewAccessibilityController.swift
@@ -0,0 +1,45 @@
+//
+//  ChartOverviewAccessibilityController.swift
+//  EmotionCode
+//
+//  Created by Andre Lami on 31/08/16.
+//  Copyright © 2016 DiscoverHealing.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChartOverviewAccessibilityController {
+}
+
+// MARK: Setup Accessibility
+
+extension ChartOverviewAccessibilityController {
+    static func setupAccessibilit(forChartOverviewView view: UIView) {
+        view.accessibilityIdentifier = ChartOverviewAccessibilityController.chartOverviewViewIdentifier
+    }
+
+    static func setupAccessibility(forColumnHeader view: UIView, forColumn column: ChartColumn, atIndex columnIndex: Int) {
+        let idenitifer = "\(ChartOverviewAccessibilityController.columnHeaderIdentifierPrefix)_\(columnIndex)"
+        view.accessibilityIdentifier = idenitifer
+    }
+
+    static func setupAccessibility(forRowCounterView view: UIView, atRowIndex rowIndex: Int) {
+        let idenitifer = "\(ChartOverviewAccessibilityController.rowCounterIdentifierPrefix)_\(rowIndex)"
+        view.accessibilityIdentifier = idenitifer
+    }
+
+    static func setupAccessibility(forRowCell view: UIView, forRow row: ChartRow, atRowPosition rowPosition: ChartRowPosition) {
+        let idenitifer = "\(ChartOverviewAccessibilityController.rowCellIdentifierPrefix)_\(rowPosition.columnIndex)_\(rowPosition.rowIndex)"
+        view.accessibilityIdentifier = idenitifer
+    }
+}
+
+// MARK: Constants
+
+extension ChartOverviewAccessibilityController {
+    static let chartOverviewViewIdentifier = "ChartOverviewView"
+    static let columnHeaderIdentifierPrefix = "ColumnHeader"
+    static let rowCounterIdentifierPrefix = "RowCounter"
+    static let rowCellIdentifierPrefix = "RowCell"
+}
