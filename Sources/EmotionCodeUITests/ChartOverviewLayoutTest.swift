9e638d0d66723317f4dc7a5cabeaa56bd88182c0
Andre Lami <andre.lami.work@gmail.com>
2016-09-01 10:47:19 +0300

    Add UI tests for chart overview layout

---
+++ Sources/EmotionCodeUITests/ChartOverviewLayoutTest.swift
@@ -0,0 +1,132 @@
+//
+//  ChartOverviewLayoutTest.swift
+//  EmotionCode
+//
+//  Created by Andre Lami on 31/08/16.
+//  Copyright © 2016 DiscoverHealing.com. All rights reserved.
+//
+
+import XCTest
+
+// MARK: Main
+
+final class ChartOverviewLayoutTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launch()
+        sleep(1)
+        app.buttons["Chart"].tap()
+    }
+
+}
+
+// MARK: Tests
+
+extension ChartOverviewLayoutTest {
+
+    func testAllCoumnsHaveHeaders() {
+        let columnHeaders = self.getColumnHeaders()
+        XCTAssertEqual(columnHeaders.count, 2)
+    }
+
+    func testAllCoumnHeadersAreAtTheTop() {
+        for index in 0 ..< 2 {
+            let columnHeader = app.collectionViews.first.childrenMatchingType(.Other).matchingIdentifier("ColumnHeader_\(index)")
+            let rowCellElement = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_\(index)_0")
+
+            let result = columnHeader.element.frame.maxY >= rowCellElement.element.frame.minY
+            XCTAssertEqual(result, true)
+        }
+    }
+
+    func testRowHasCounterElement() {
+        let rowCounterElement = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCounter_0")
+        XCTAssertEqual(rowCounterElement.count, 1)
+    }
+
+    func testRowCounterIsOnTheLeftOfRowCell() {
+        let rowCounterElement = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCounter_0")
+        let rowCellElement = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_0_0")
+
+        let result = rowCounterElement.element.frame.maxX <= rowCellElement.element.frame.minX
+
+        XCTAssertEqual(result, true)
+    }
+
+    func testRowCounterIsAlignedHorizontally() {
+        let rowCounterElement = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCounter_0")
+        let rowCellElement = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_0_0")
+
+        let topAligned = rowCounterElement.element.frame.minY == rowCellElement.element.frame.minY
+        let botAligned = rowCounterElement.element.frame.maxY == rowCellElement.element.frame.maxY
+
+        XCTAssertEqual(topAligned && botAligned, true)
+    }
+
+    func testRowCounterIsAlignedVertically() {
+        let rowCounterElementTop = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCounter_0")
+        let rowCounterElementBottom = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCounter_1")
+
+        let leftAligned = rowCounterElementTop.element.frame.minX == rowCounterElementBottom.element.frame.minX
+        let rightAligned = rowCounterElementTop.element.frame.maxX == rowCounterElementBottom.element.frame.maxX
+
+        XCTAssertEqual(leftAligned && rightAligned, true)
+    }
+
+    func testRowCellElementsHaveHorizontalSpacing() {
+        let rowCellElementLeft = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_0_0")
+        let rowCellElementRight = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_1_0")
+
+        let result = rowCellElementLeft.element.frame.maxX < rowCellElementRight.element.frame.minX
+
+        XCTAssertEqual(result, true)
+    }
+
+    func testRowCellElementsHaveVerticalSpacing() {
+        let rowCellElementTop = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_0_0")
+        let rowCellElementBottom = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_0_1")
+
+        let result = rowCellElementTop.element.frame.maxY <= rowCellElementBottom.element.frame.minY
+
+        XCTAssertEqual(result, true)
+    }
+
+    func testRowCellElementsAreAllignedHorizontally() {
+        let rowCellElementLeft = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_0_0")
+        let rowCellElementRight = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_1_0")
+
+        let topAligned = rowCellElementLeft.element.frame.minY == rowCellElementRight.element.frame.minY
+        let bottomAligned = rowCellElementLeft.element.frame.maxY == rowCellElementRight.element.frame.maxY
+
+        XCTAssertEqual(topAligned && bottomAligned, true)
+    }
+
+    func testRowCellElementsAreAllignedVertically() {
+        let rowCellElementTop = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_0_0")
+        let rowCellElementBottom = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_0_1")
+
+        let leftAligned = rowCellElementTop.element.frame.minX == rowCellElementBottom.element.frame.minX
+        let rightAligned = rowCellElementTop.element.frame.maxX == rowCellElementBottom.element.frame.maxX
+
+        XCTAssertEqual(leftAligned && rightAligned, true)
+    }
+}
+
+
+// MARK: Helpers
+
+extension ChartOverviewLayoutTest {
+
+    func getColumnHeaders() -> XCUIElementQuery {
+        let childerent = app.collectionViews.first.childrenMatchingType(.Other)
+        let columnHeadersPreddicate = NSPredicate { columnHeader, _ -> Bool in
+            guard let columnHeader = columnHeader as? XCUIElementAttributes else { return false }
+            return columnHeader.identifier.hasPrefix("ColumnHeader")
+        }
+
+        let columnHeaders = childerent.matchingPredicate(columnHeadersPreddicate)
+        return columnHeaders
+    }
+}
