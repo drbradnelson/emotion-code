2a1574e971c1278d0025abb1bc6598bfad30f4e8
Andre Lami <andre.lami.work@gmail.com>
2016-09-02 11:07:12 +0300

    Remove unsused ChartOverviewCollectionLayoutOther.swift file

--- Sources/EmotionCode/ChartOverviewCollectionLayoutOther.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ChartOverviewCollectionLayoutOther.swift
-//  EmotionCode
-//
-//  Created by Andre Lami on 30/08/16.
-//  Copyright © 2016 DiscoverHealing.com. All rights reserved.
-//
-
-import Foundation
