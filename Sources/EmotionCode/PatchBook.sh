3d622308f409035612bd180269037a226800f55a
Russ Kamalov <ruslankmlv@gmail.com>
2016-09-05 11:29:36 -0700

    Add patching Spanish version to script

--- Sources/EmotionCode/PatchBook.sh
+++ Sources/EmotionCode/PatchBook.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash -e
 
+UNLOCALIZED_RESOURCES_FOLDER_PATH=${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}
+ENGLISH_LOCALIZATION_FOLDER_PATH=$UNLOCALIZED_RESOURCES_FOLDER_PATH/en.lproj
+SPANISH_LOCALIZATION_FOLDER_PATH=$UNLOCALIZED_RESOURCES_FOLDER_PATH/es.lproj
+
+mkdir -p $ENGLISH_LOCALIZATION_FOLDER_PATH
+mkdir -p $SPANISH_LOCALIZATION_FOLDER_PATH
+
+#English version
 pushd ../
 patch Resources/English/The\ Emotion\ Code\,\ November\ 2015.html  --input=Sources/EmotionCode/BookPatches/English/book-add-chapters-en.patch --output=Sources/EmotionCode/book-with-chapters.html
 popd
@@ -8,11 +16,6 @@ sed -E -e's,<a href="">(.*)</a>,\1,g' -e's,<span.*> +</span>,,g' <EmotionCode/bo
 
 rm EmotionCode/book-with-chapters.html
 rm EmotionCode/book-no-chapter-numbers.html
-
-mkdir -p ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/en.lproj/
-
-UNLOCALIZED_RESOURCES_FOLDER_PATH=${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}
-ENGLISH_LOCALIZATION_FOLDER_PATH=$UNLOCALIZED_RESOURCES_FOLDER_PATH/en.lproj
 
 patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/English/chapter1-en.patch --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter1.html
 patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/English/chapter2-en.patch --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter2.html
@@ -31,4 +34,29 @@ patch $ENGLISH_LOCALIZATION_FOLDER_PATH/Resources/css/idGeneratedStyles.css --in
 
 rm EmotionCode/book-fixed.html
 
+#Spanish version
+pushd ../
+patch Resources/Spanish/The\ Emotion\ Code\,\ Revision\ 9-13.html  --input=Sources/EmotionCode/BookPatches/Spanish/book-add-chapters-es.patch --output=Sources/EmotionCode/book-with-chapters.html
+popd
+patch EmotionCode/book-with-chapters.html --input=EmotionCode/BookPatches/Spanish/book-remove-chapter-numbers-es.patch --output=EmotionCode/book-no-chapter-numbers.html
+sed -E -e's,<a href="">(.*)</a>,\1,g' -e's,<span.*> +</span>,,g' <EmotionCode/book-no-chapter-numbers.html >EmotionCode/book-fixed.html
 
+rm EmotionCode/book-with-chapters.html
+rm EmotionCode/book-no-chapter-numbers.html
+
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter1-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter1.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter2-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter2.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter3-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter3.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter4-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter4.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter5-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter5.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter6-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter6.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter7-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter7.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter8-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter8.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter9-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter9.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter10-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter10.html
+patch EmotionCode/book-fixed.html --input=EmotionCode/BookPatches/Spanish/chapter11-es.patch --output=$SPANISH_LOCALIZATION_FOLDER_PATH/about-author.html
+
+cp -r ../Resources/Spanish/Resources $SPANISH_LOCALIZATION_FOLDER_PATH
+patch $SPANISH_LOCALIZATION_FOLDER_PATH/Resources/css/idGeneratedStyles.css --input=EmotionCode/BookPatches/Spanish/css_es.patch
+
+rm EmotionCode/book-fixed.html
