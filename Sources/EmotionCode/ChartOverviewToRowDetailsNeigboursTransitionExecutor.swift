80ec86afd7ea6307abeae2498af48d9d2efe0e93
Andre Lami <andre.lami.work@gmail.com>
2016-09-07 10:14:52 +0300

    Refactor chart overview to row details neighbours transition

--- Sources/EmotionCode/ChartOverviewToRowDetailsNeigboursTransitionExecutor.swift
+++ Sources/EmotionCode/ChartOverviewToRowDetailsNeigboursTransitionExecutor.swift
@@ -1,79 +1,100 @@
-//
-//  ChartOverviewToRowDetailsNeigboursTransitionExecutor.swift
-//  EmotionCode
-//
-//  Created by Andre Lami on 02/09/16.
-//  Copyright © 2016 DiscoverHealing.com. All rights reserved.
-//
-
-import Foundation
 import UIKit
 
 extension ChartOverviewToRowDetailsTransition {
-    
-    func createNeighboursTransition(forData data:ChartOverviewToRowDetailsTransitionData) -> TransitionExecutor {
-        let chatRowPosition = rowDetailsController.chartRowPosition!
-        let indexPath = overviewController.chartAdapter.indexPath(forRowPosition: chatRowPosition)
-        let overviewRowCell = data.overviewController.chartView.cellForItemAtIndexPath(indexPath) as! ChartOverviewRowCell
-        
-        let overviewVisibleViews = self.fetchOverviewVisibleViews(forData: data)
-        
-        let overviewTransitionRowCellFrame = self.calculateOverviewTransitionRowCellBounds(forData: data)
-        let rowDetailsFrame = self.calculateRowDetailsBounds(forData: data)
-        
-        let sizeAspect =  self.calculateItemsSizeAspec(forData: data)
-        
+
+    private struct NeigboursTransitionInfo {
+        init() {}
+
+        var chatRowPosition: ChartRowPosition!
+        var overviewRowCell: ChartOverviewRowCell!
+        var overviewVisibleViews: [UIView]!
+
+        var overviewTransitionRowCellFrame: CGRect!
+
+        var sizeAspect: CGFloat!
+        var transform: CGAffineTransform!
+    }
+
+    func createNeighboursTransition(forData data: ChartOverviewToRowDetailsTransitionData) -> TransitionExecutor {
+        let info = self.createTransitionInfo(forData: data)
         var executors = [TransitionExecutor]()
-        
-        let translationVector = CGPoint.init(x: rowDetailsFrame.midX - overviewTransitionRowCellFrame.midX, y: rowDetailsFrame.midY - overviewTransitionRowCellFrame.midY)
-        let translation = CGAffineTransformMakeTranslation(translationVector.x, translationVector.y)
-        let transform = CGAffineTransformScale(translation, sizeAspect, sizeAspect)
-        
-        for overviewVisibleView in overviewVisibleViews {
-            
-            if overviewVisibleView == overviewRowCell {
+        for overviewVisibleView in info.overviewVisibleViews {
+
+            if overviewVisibleView == info.overviewRowCell {
                 continue
             }
-            
-            let overviewVisibleSnapshotContainer = UIView.init()
-            let overviewVisibleViewFrame = data.overviewController.chartView.convertRect(overviewVisibleView.frame, toView: data.containerView)
 
-            let overviewVisibleViewAnchorPoint = self.calculateOverviewVisibleViewAnchorPoint(forData: data, view: overviewVisibleView, andTransitionRowCellFrame: overviewTransitionRowCellFrame)
-            
-            data.containerView.addSubview(overviewVisibleSnapshotContainer)
-            
-            let overviewVisibleSnapshot = overviewVisibleView.snapshotViewAfterScreenUpdates(self.value(data, forward: false, back: true))
-            overviewVisibleSnapshotContainer.addSubview(overviewVisibleSnapshot)
-            overviewVisibleSnapshot.frame = CGRect.init(origin:CGPoint.init(x: 0, y: 0), size: overviewVisibleViewFrame.size)
-            
-            overviewVisibleSnapshotContainer.layer.anchorPoint = overviewVisibleViewAnchorPoint
-            overviewVisibleSnapshotContainer.frame = overviewVisibleViewFrame
-
-            
-            let prepareBlock = {
-                overviewVisibleSnapshotContainer.transform = self.value(data, forward: CGAffineTransformIdentity, back: transform)
-                overviewVisibleSnapshotContainer.alpha = self.value(data, forward: 1, back: 0)
-            }
-
-            let animationBlock = {
-                overviewVisibleSnapshotContainer.transform = self.value(data, forward: transform, back: CGAffineTransformIdentity)
-                overviewVisibleSnapshotContainer.alpha = self.value(data, forward: 0, back: 1)
-            }
-            
-            let completionBlock = { (cancelled: Bool) -> () in
-                overviewVisibleSnapshotContainer.removeFromSuperview()
-                overviewVisibleView.hidden = false
-            }
-            
-            let executor = TransitionExecutorFactory.transitionExecutor(withPrepareBlock: prepareBlock, executeBlock: animationBlock, completionBlock: completionBlock)
+            let overviewTransitionContainer = self.createOverviewTransitionContainer(forView: overviewVisibleView, data: data, andInfo: info)
+            let executor = self.createExecutor(forOverviewTransitionContainer: overviewTransitionContainer, data: data, andInfo: info)
             executors.append(executor)
         }
-        
+
         let transitionExecutor = TransitionExecutorFactory.transitionExecutor(withExecutors: executors)
         return transitionExecutor
     }
+}
 
-    private func calculateRowDetailsBounds(forData data:ChartOverviewToRowDetailsTransitionData) -> CGRect {
+// MARK: Helpers
+
+extension ChartOverviewToRowDetailsTransition {
+    private func createTransitionInfo(forData data: ChartOverviewToRowDetailsTransitionData) -> NeigboursTransitionInfo {
+        var info: NeigboursTransitionInfo = NeigboursTransitionInfo()
+        info.chatRowPosition = rowDetailsController.chartRowPosition!
+        let indexPath = overviewController.chartAdapter.indexPath(forRowPosition: info.chatRowPosition)
+        info.overviewRowCell = data.overviewController.chartView.cellForItemAtIndexPath(indexPath) as! ChartOverviewRowCell
+
+        info.overviewVisibleViews = self.fetchOverviewVisibleViews(forData: data)
+
+        info.overviewTransitionRowCellFrame = self.calculateOverviewTransitionRowCellBounds(forData: data)
+        let rowDetailsFrame = self.calculateRowDetailsBounds(forData: data)
+
+        info.sizeAspect =  self.calculateItemsSizeAspec(forData: data)
+
+        let translationVector = CGPoint.init(x: rowDetailsFrame.midX - info.overviewTransitionRowCellFrame.midX, y: rowDetailsFrame.midY - info.overviewTransitionRowCellFrame.midY)
+        let translation = CGAffineTransformMakeTranslation(translationVector.x, translationVector.y)
+        info.transform = CGAffineTransformScale(translation, info.sizeAspect, info.sizeAspect)
+        return info
+    }
+
+    private func createOverviewTransitionContainer(forView overviewView: UIView, data: ChartOverviewToRowDetailsTransitionData, andInfo info: NeigboursTransitionInfo) -> UIView {
+        let overviewVisibleTransitionContainer = UIView.init()
+        let overviewVisibleViewFrame = data.overviewController.chartView.convertRect(overviewView.frame, toView: data.containerView)
+
+        let overviewVisibleViewAnchorPoint = self.calculateOverviewVisibleViewAnchorPoint(forData: data, view: overviewView, andTransitionRowCellFrame: info.overviewTransitionRowCellFrame)
+
+        data.containerView.addSubview(overviewVisibleTransitionContainer)
+
+        let overviewVisibleSnapshot = overviewView.snapshotViewAfterScreenUpdates(self.value(data, forward: false, back: true))
+        overviewVisibleTransitionContainer.addSubview(overviewVisibleSnapshot)
+        overviewVisibleSnapshot.frame = CGRect.init(origin:CGPoint.init(x: 0, y: 0), size: overviewVisibleViewFrame.size)
+
+        overviewVisibleTransitionContainer.layer.anchorPoint = overviewVisibleViewAnchorPoint
+        overviewVisibleTransitionContainer.frame = overviewVisibleViewFrame
+
+        return overviewVisibleTransitionContainer
+    }
+
+    private func createExecutor(forOverviewTransitionContainer container: UIView, data: ChartOverviewToRowDetailsTransitionData, andInfo info: NeigboursTransitionInfo) -> TransitionExecutor {
+        let prepareBlock = {
+            container.transform = self.value(data, forward: CGAffineTransformIdentity, back: info.transform)
+            container.alpha = self.value(data, forward: 1, back: 0)
+        }
+
+        let animationBlock = {
+            container.transform = self.value(data, forward: info.transform, back: CGAffineTransformIdentity)
+            container.alpha = self.value(data, forward: 0, back: 1)
+        }
+
+        let completionBlock = { (cancelled: Bool) -> () in
+            container.removeFromSuperview()
+        }
+
+        let executor = TransitionExecutorFactory.transitionExecutor(withPrepareBlock: prepareBlock, executeBlock: animationBlock, completionBlock: completionBlock)
+
+        return executor
+    }
+
+    private func calculateRowDetailsBounds(forData data: ChartOverviewToRowDetailsTransitionData) -> CGRect {
         let rowDetailsItems = data.rowDetailsController.rowDetailsView.visibleCells()
         let rowDetailsTopItemView = rowDetailsItems.first!
         let rowDetailsBotItemView = rowDetailsItems.last!
@@ -84,7 +105,7 @@ extension ChartOverviewToRowDetailsTransition {
         return frame
     }
 
-    private func calculateOverviewTransitionRowCellBounds(forData data:ChartOverviewToRowDetailsTransitionData) -> CGRect {
+    private func calculateOverviewTransitionRowCellBounds(forData data: ChartOverviewToRowDetailsTransitionData) -> CGRect {
         let chatRowPosition = rowDetailsController.chartRowPosition!
         let indexPath = data.overviewController.chartAdapter.indexPath(forRowPosition: chatRowPosition)
         let overviewRowCell = data.overviewController.chartView.cellForItemAtIndexPath(indexPath) as! ChartOverviewRowCell
@@ -95,7 +116,7 @@ extension ChartOverviewToRowDetailsTransition {
         return frame
     }
 
-    private func calculateItemsSizeAspec(forData data:ChartOverviewToRowDetailsTransitionData) -> CGFloat {
+    private func calculateItemsSizeAspec(forData data: ChartOverviewToRowDetailsTransitionData) -> CGFloat {
         let rowDetailsItems = data.rowDetailsController.rowDetailsView.visibleCells()
         let rowDetailsItem = rowDetailsItems.first!
 
@@ -107,27 +128,25 @@ extension ChartOverviewToRowDetailsTransition {
         let sizeAspect = rowDetailsItem.frame.width / overviewItem.frame.width
         return sizeAspect
     }
-    
-    private func fetchOverviewVisibleViews(forData data:ChartOverviewToRowDetailsTransitionData) -> [UIView] {
+
+    private func fetchOverviewVisibleViews(forData data: ChartOverviewToRowDetailsTransitionData) -> [UIView] {
         let overviewVisibleItems: [UIView] = data.overviewController.chartView.visibleCells()
         let overviewVisibleColumnHeaders: [UIView] = data.overviewController.chartView.visibleSupplementaryViewsOfKind(ChartOverviewCollectionLayout.kColumnHeaderElementIdentifier)
         let overviewVisibleCounters: [UIView] = data.overviewController.chartView.visibleSupplementaryViewsOfKind(ChartOverviewCollectionLayout.kRowCounterElementIdentifier)
-        
+
         var overviewVisibleViews = [UIView]()
         overviewVisibleViews.appendContentsOf(overviewVisibleItems)
         overviewVisibleViews.appendContentsOf(overviewVisibleColumnHeaders)
         overviewVisibleViews.appendContentsOf(overviewVisibleCounters)
-        
-        return overviewVisibleViews
 
+        return overviewVisibleViews
     }
 
-    private func calculateOverviewVisibleViewAnchorPoint(forData data:ChartOverviewToRowDetailsTransitionData, view: UIView, andTransitionRowCellFrame frame:CGRect) -> CGPoint {
+    private func calculateOverviewVisibleViewAnchorPoint(forData data: ChartOverviewToRowDetailsTransitionData, view: UIView, andTransitionRowCellFrame frame: CGRect) -> CGPoint {
         let globAnchorPoint = CGPoint.init(x: frame.midX, y: frame.midY)
         let localAnchorPoint = data.containerView.convertPoint(globAnchorPoint, toView: view)
         let normAnchorPoint = CGPoint.init(x: localAnchorPoint.x / view.bounds.width, y: localAnchorPoint.y / view.bounds.height)
 
         return normAnchorPoint
-
     }
 }
