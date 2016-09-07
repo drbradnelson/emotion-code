d36998d3540be54cb0ce3d8295f0e2db302e7632
AndreLami <andre.lami.work@gmail.com>
2016-09-07 16:35:00 +0300

    Add chart item details view controller

---
+++ Sources/EmotionCode/ChartItemDetailsViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// MARK: Main
+
+class ChartItemDetailsViewController: UIViewController {
+
+    @IBOutlet private(set) weak var nameLabel: UILabel!
+    @IBOutlet private(set) weak var detailsLabel: UILabel!
+    
+}
