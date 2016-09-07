0262bc26763a2dcc14496184a3b3f08174dc2dfd
Andre Lami <andre.lami.work@gmail.com>
2016-09-07 10:45:18 +0300

    Add chart row details accessibility controller

--- Sources/EmotionCode/ChartRowDetailsAccessibilityController.swift
+++ Sources/EmotionCode/ChartRowDetailsAccessibilityController.swift
@@ -1,25 +1,25 @@
 import UIKit
 
-final class ChartRowDetailsAccessibilityController {
-
-}
+final class ChartRowDetailsAccessibilityController {}
 
 // MARK: Setup Accessibility
 
 extension ChartRowDetailsAccessibilityController {
+
     static func setupAccessibilit(forChartOverviewView view: UIView) {
-        view.accessibilityIdentifier = ChartOverviewAccessibilityController.chartOverviewViewIdentifier
+        view.accessibilityIdentifier = self.chartRowDetailsViewIdentifier
     }
 
     static func setupAccessibility(forRowCounterView view: UIView, atRowIndex rowIndex: Int) {
-        let idenitifer = "\(ChartRowDetailsAccessibilityController.rowCellIdentifierPrefix)_\(rowIndex)"
+        let idenitifer = "\(self.rowCellIdentifierPrefix)_\(rowIndex)"
         view.accessibilityIdentifier = idenitifer
     }
+
 }
 
 // MARK: Constants
 
-extension ChartRowDetailsAccessibilityController {
-    static let chartOverviewViewIdentifier = "ChartRowDetailsView"
+private extension ChartRowDetailsAccessibilityController {
+    static let chartRowDetailsViewIdentifier = "ChartRowDetailsView"
     static let rowCellIdentifierPrefix = "RowCell"
 }
