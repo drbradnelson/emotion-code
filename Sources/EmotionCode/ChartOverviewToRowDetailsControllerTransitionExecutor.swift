5289f7ffe67117dcca26fbe89f14842e2ab1cb39
Andre Lami <andre.lami.work@gmail.com>
2016-09-07 10:15:10 +0300

    Refactor chart overview to row details controllers transition

--- Sources/EmotionCode/ChartOverviewToRowDetailsControllerTransitionExecutor.swift
+++ Sources/EmotionCode/ChartOverviewToRowDetailsControllerTransitionExecutor.swift
@@ -1,47 +1,59 @@
-//
-//  ChartOverviewToRowDetailsControllerTransitionExecutor.swift
-//  EmotionCode
-//
-//  Created by Andre Lami on 02/09/16.
-//  Copyright © 2016 DiscoverHealing.com. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
 extension ChartOverviewToRowDetailsTransition {
-    
-    func createControllersTransition(forData data:ChartOverviewToRowDetailsTransitionData) -> TransitionExecutor {
+
+    func createControllersTransition(forData data: ChartOverviewToRowDetailsTransitionData) -> TransitionExecutor {
+        let overviewControllerTransition = self.createOverviewControllerExecutor(forData: data)
+        let rowDetailsControllerTransition = self.createRowDetailsControllerExecutor(forData: data)
+
+        let transitionExecutor = TransitionExecutorFactory.transitionExecutor(withExecutors: [overviewControllerTransition, rowDetailsControllerTransition])
+
+        return transitionExecutor
+    }
+
+    private func createOverviewControllerExecutor(forData data: ChartOverviewToRowDetailsTransitionData) -> TransitionExecutor {
         let overviewTransitionCoverView = UIView.init()
         overviewTransitionCoverView.frame = data.containerView.bounds
         overviewTransitionCoverView.backgroundColor = data.overviewController.view.backgroundColor
-        
+
         let prepareBlock = {
-            
-            self.rowDetailsController.rowDetailsView.hidden = true
-            
             if data.direction == TransitionDirection.Forward {
-                data.containerView.addSubview(data.rowDetailsController.view)
-                data.rowDetailsController.view.alpha = 0
-                data.containerView.insertSubview(overviewTransitionCoverView, aboveSubview: self.overviewController.view)
-                
+                data.containerView.insertSubview(overviewTransitionCoverView, aboveSubview: data.overviewController.view)
             } else {
-                self.containerView.insertSubview(overviewTransitionCoverView, belowSubview: self.rowDetailsController.view)
+                data.containerView.insertSubview(overviewTransitionCoverView, belowSubview: data.rowDetailsController.view)
                 data.containerView.insertSubview(data.overviewController.view, belowSubview: overviewTransitionCoverView)
             }
         }
-        
-        let animationBlock = {
-            self.rowDetailsController.view.alpha = self.value(data, forward: 1, back: 0)
-        }
-        
+
         let completionBlock = { (cancelled: Bool) -> () in
-            self.rowDetailsController.rowDetailsView.hidden = false
             overviewTransitionCoverView.removeFromSuperview()
         }
-        
+
+        let transitionExecutor = TransitionExecutorFactory.transitionExecutor(withPrepareBlock: prepareBlock, executeBlock: {}, completionBlock: completionBlock)
+
+        return transitionExecutor
+    }
+
+    private func createRowDetailsControllerExecutor(forData data: ChartOverviewToRowDetailsTransitionData) -> TransitionExecutor {
+        let prepareBlock = {
+            data.rowDetailsController.rowDetailsView.hidden = true
+            if data.direction == TransitionDirection.Forward {
+                data.containerView.addSubview(data.rowDetailsController.view)
+                data.rowDetailsController.view.alpha = 0
+            }
+        }
+
+        let animationBlock = {
+            data.rowDetailsController.view.alpha = self.value(data, forward: 1, back: 0)
+        }
+
+        let completionBlock = { (cancelled: Bool) -> () in
+            data.rowDetailsController.rowDetailsView.hidden = false
+        }
+
         let transitionExecutor = TransitionExecutorFactory.transitionExecutor(withPrepareBlock: prepareBlock, executeBlock: animationBlock, completionBlock: completionBlock)
-        
+
         return transitionExecutor
     }
 }
