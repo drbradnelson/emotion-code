c115a229fba15129f425de40b3e8c17964e03185
Andre Lami <andre.lami.work@gmail.com>
2016-09-07 10:47:18 +0300

    Add chart overview transition UI test

---
+++ Sources/EmotionCodeUITests/ChartOverviewTransitionTest.swift
@@ -0,0 +1,31 @@
+import XCTest
+
+// MARK: Main
+
+final class ChartOverviewTransitionTest: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launch()
+        sleep(1)
+        app.buttons["Chart"].tap()
+    }
+}
+
+// MARK: Tests
+
+extension ChartOverviewTransitionTest {
+
+    func testChartOverviewTransition() {
+
+        let rowCellElement = app.collectionViews.first.childrenMatchingType(.Any).matchingIdentifier("RowCell_0_0")
+        rowCellElement.element.tap()
+
+        sleep(1)
+
+        let view = app.collectionViews.matchingIdentifier("ChartRowDetailsView")
+
+        XCTAssertEqual(view.count, 1)
+    }
+}
