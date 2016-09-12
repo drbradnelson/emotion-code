827c736f9f7b30423e33fd7c0f7a3515a153ab89
Andre Lami <andre.lami.work@gmail.com>
2016-09-12 11:21:12 +0300

    Remove unnecessary self

--- Sources/EmotionCode/ChartOverviewAccessibilityController.swift
+++ Sources/EmotionCode/ChartOverviewAccessibilityController.swift
@@ -7,21 +7,21 @@ final class ChartOverviewAccessibilityController {}
 extension ChartOverviewAccessibilityController {
 
     static func setupAccessibility(forChartOverviewView view: UIView) {
-        view.accessibilityIdentifier = self.chartOverviewViewIdentifier
+        view.accessibilityIdentifier = chartOverviewViewIdentifier
     }
 
     static func setupAccessibility(forColumnHeader view: UIView, forColumn column: ChartColumn, atIndex columnIndex: Int) {
-        let identifier = "\(self.columnHeaderIdentifierPrefix)_\(columnIndex)"
+        let identifier = "\(columnHeaderIdentifierPrefix)_\(columnIndex)"
         view.accessibilityIdentifier = identifier
     }
 
     static func setupAccessibility(forRowCounterView view: UIView, atRowIndex rowIndex: Int) {
-        let identifier = "\(self.rowCounterIdentifierPrefix)_\(rowIndex)"
+        let identifier = "\(rowCounterIdentifierPrefix)_\(rowIndex)"
         view.accessibilityIdentifier = identifier
     }
 
     static func setupAccessibility(forRowCell view: UIView, forRow row: ChartRow, atRowPosition rowPosition: ChartRowPosition) {
-        let identifier = "\(self.rowCellIdentifierPrefix)_\(rowPosition.columnIndex)_\(rowPosition.rowIndex)"
+        let identifier = "\(rowCellIdentifierPrefix)_\(rowPosition.columnIndex)_\(rowPosition.rowIndex)"
         view.accessibilityIdentifier = identifier
     }
 
