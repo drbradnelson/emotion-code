754f5eb778cf4b63918a0539bd58bae9771ab9e3
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-09-26 12:03:27 +0300

    Create base values to configure ChartCollectionViewController

--- Sources/EmotionCode/ChartCollectionViewController.swift
+++ Sources/EmotionCode/ChartCollectionViewController.swift
@@ -2,4 +2,8 @@ import UIKit
 
 class ChartCollectionViewController: UICollectionViewController {
 	
+	private let cellReuseIdentifier = "ItemCell"
+	private let chart = ChartController().chart
+
+	
 }
