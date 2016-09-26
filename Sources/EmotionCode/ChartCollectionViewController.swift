b11cf698e8f4019ebe96b00cc9b51863eb63ad27
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-09-26 12:04:53 +0300

    Set numberOfSections for ChartCollectionVC

--- Sources/EmotionCode/ChartCollectionViewController.swift
+++ Sources/EmotionCode/ChartCollectionViewController.swift
@@ -1,9 +1,15 @@
 import UIKit
 
 class ChartCollectionViewController: UICollectionViewController {
-	
+
 	private let cellReuseIdentifier = "ItemCell"
 	private let chart = ChartController().chart
 
-	
+	// MARK: Collection view data source
+
+	override func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return chart.columns.reduce(0) { maxRowCount, column in
+			max(maxRowCount, column.rows.count)
+		}
+	}
 }
