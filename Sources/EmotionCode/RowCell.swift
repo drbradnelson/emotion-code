40f66de0572994e6db644c2854345e43c6976104
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-10-02 14:56:19 +0300

    Configure RowCell

--- Sources/EmotionCode/RowCell.swift
+++ Sources/EmotionCode/RowCell.swift
@@ -2,4 +2,21 @@ import UIKit
 
 class RowCell: UICollectionViewCell {
 
+	@IBOutlet private var stackView: UIStackView!
+
+	var row: Chart.Row!
+
+	override func layoutSubviews() {
+		super.layoutSubviews()
+
+		for view in stackView.arrangedSubviews {
+			view.removeFromSuperview()
+		}
+
+		for item in row.items {
+			let titleView = TitleView()
+			titleView.titleLabel.text = item.title
+			stackView.addArrangedSubview(titleView)
+		}
+	}
 }
--- Sources/EmotionCode/TitleView.swift
+++ Sources/EmotionCode/TitleView.swift
@@ -1,17 +1,17 @@
 import UIKit
 
 class TitleView: UIView {
-	
+
 	let titleLabel = UILabel()
-	
+
 	override func layoutSubviews() {
 		super.layoutSubviews()
 		titleLabel.font = .preferredFont(forTextStyle: .caption1)
 		titleLabel.frame = bounds
 		titleLabel.textAlignment = .center
 		addSubview(titleLabel)
-		
+
 		backgroundColor = .lightGray
 	}
-	
+
 }
