af1b5d98baa15df1498cf1ae651918991fe1b3d9
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-10-04 12:06:31 +0300

    Improve layout set in ChartItemVC

--- Sources/EmotionCode/ChartItemViewController.swift
+++ Sources/EmotionCode/ChartItemViewController.swift
@@ -3,14 +3,19 @@ import UIKit
 final class ChartItemViewController: UIViewController {
 
 	@IBOutlet private var itemView: UIView!
-	@IBOutlet private var titleLabel: UILabel!
-	@IBOutlet private var descriptionTextView: UITextView!
+
+	@IBOutlet private var titleLabel: UILabel! {
+		didSet {
+			titleLabel?.text = item.title
+		}
+	}
+
+	@IBOutlet private var descriptionTextView: UITextView! {
+		didSet {
+			descriptionTextView?.text = item.description
+		}
+	}
 
 	var item: Chart.Item!
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		titleLabel.text = item.title
-		descriptionTextView.text = item.description
-	}
 }
