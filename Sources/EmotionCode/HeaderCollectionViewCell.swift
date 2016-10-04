71646e3de961c3329e0298c240a600a989575f59
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-10-04 12:31:59 +0300

    Improve HeaderCollectionViewCell layout and initialization

--- Sources/EmotionCode/HeaderCollectionViewCell.swift
+++ Sources/EmotionCode/HeaderCollectionViewCell.swift
@@ -2,15 +2,25 @@ import UIKit
 
 final class HeaderCollectionViewCell: UICollectionViewCell {
 
-	let titleLabel = UILabel()
+	private(set) var titleLabel: UILabel!
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		isUserInteractionEnabled = false
+
+		titleLabel = UILabel()
+		titleLabel.font = .preferredFont(forTextStyle: .body)
+		titleLabel.textAlignment = .center
+		addSubview(titleLabel)
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 
 	override func layoutSubviews() {
 		super.layoutSubviews()
-		isUserInteractionEnabled = false
-		titleLabel.font = .preferredFont(forTextStyle: .body)
 		titleLabel.frame = bounds
-		titleLabel.textAlignment = .center
-		addSubview(titleLabel)
 	}
 
 }
