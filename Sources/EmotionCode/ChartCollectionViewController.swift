6f2f8f6c24414ab0256d5d2bd03912654b378a76
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-10-14 14:18:23 +0300

    Configure flow layout in ChartCollectionViewController

--- Sources/EmotionCode/ChartCollectionViewController.swift
+++ Sources/EmotionCode/ChartCollectionViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-final class ChartCollectionViewController: UICollectionViewController {
+final class ChartCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     private let chart = ChartController().chart
 
@@ -18,13 +18,25 @@ final class ChartCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ItemCollectionViewCell.reuseIdentifier, for: indexPath) as! ItemCollectionViewCell
-        
+
         let columnIndex = (indexPath.item + 2) % 2
         let itemIndex = (indexPath.item - columnIndex) / 2
-        
+
         let title = chart.rows[indexPath.section].columns[columnIndex].items[itemIndex].title
         cell.configure(title: title)
         return cell
+    }
+
+    // MARK: Collection view delegate flow layout
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfColumns: CGFloat = 2
+
+        let layout = collectionViewLayout as! UICollectionViewFlowLayout
+        let spacing = layout.sectionInset.left + layout.sectionInset.right + layout.minimumInteritemSpacing * (numberOfColumns - 1)
+        let width = (collectionView.bounds.width - spacing) / numberOfColumns
+
+        return CGSize(width: width, height: 30)
     }
 
 }
