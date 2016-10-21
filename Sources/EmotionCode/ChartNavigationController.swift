e9cc90dcd49b7040f0028e52a1c06d1be7fc113b
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-10-21 10:00:05 +0300

    Clean navigation controller, add padding between sections in ChartColumnViewController

--- Sources/EmotionCode/ChartNavigationController.swift
+++ Sources/EmotionCode/ChartNavigationController.swift
@@ -10,17 +10,8 @@ final class ChartNavigationController: UINavigationController, UINavigationContr
     // MARK: Navigation controller delegate
 
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-        if let vc = viewController as? ChartViewController {
-            vc.collectionView?.dataSource = vc
-        } else if let vc = viewController as? ChartColumnViewController {
-            vc.collectionView?.delegate = vc
-        }
-    }
-
-    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-        if let vc = viewController as? ChartColumnViewController {
-            vc.collectionView?.dataSource = vc
-        }
+        let vc = viewController as? ChartColumnViewController
+        vc?.collectionView?.delegate = vc
     }
 
 }
