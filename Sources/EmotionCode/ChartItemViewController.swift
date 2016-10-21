5cf1287781121c7a4e7d48820b65889b96fb9ba3
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-10-21 11:53:05 +0300

    Configure dataSource in ChartItemViewController

--- Sources/EmotionCode/ChartItemViewController.swift
+++ Sources/EmotionCode/ChartItemViewController.swift
@@ -2,4 +2,15 @@ import UIKit
 
 final class ChartItemViewController: UICollectionViewController {
 
+    var item: Chart.Item!
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ItemCollectionViewCell.reuseIdentifier, for: indexPath) as! ItemCollectionViewCell
+        cell.configure(title: item.title)
+        return cell
+    }
 }
