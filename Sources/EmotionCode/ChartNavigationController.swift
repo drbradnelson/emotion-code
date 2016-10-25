c6890521f4547feb4129d19ed7e9e98cbf5ce11c
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-10-25 10:46:48 +0300

    Set navigation title in ChartNavigationController

--- Sources/EmotionCode/ChartNavigationController.swift
+++ Sources/EmotionCode/ChartNavigationController.swift
@@ -14,6 +14,17 @@ final class ChartNavigationController: UINavigationController, UINavigationContr
         vc?.collectionView?.delegate = vc
 
         vc?.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+
+        switch vc {
+        case let vc as ChartColumnViewController:
+            let column = (vc.selectedSection + 2) % 2
+            let row = vc.selectedSection / 2 + 1
+            let columnNames = ["A", "B"]
+            vc.navigationItem.title = "Row \(row) - Column \(columnNames[column])"
+        case let vc as ChartItemViewController:
+            vc.navigationItem.title = vc.item.title
+        default: break
+        }
     }
 
 }
