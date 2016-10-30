21f717a5d05c92f4ab1a59f8c7a53340573e5bc3
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-10-30 21:58:14 +0300

    Disable scroll for ChartColumn and ChartItem view controllers

--- Sources/EmotionCode/ChartNavigationController.swift
+++ Sources/EmotionCode/ChartNavigationController.swift
@@ -12,6 +12,7 @@ final class ChartNavigationController: UINavigationController, UINavigationContr
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         guard let destination = viewController as? UICollectionViewController else { return }
         destination.collectionView?.delegate = destination
+        destination.collectionView?.isScrollEnabled = destination is ChartViewController
 
         destination.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
 
