7379803e90a4a344fbc64e6aea9261ab56d0a542
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-10-31 21:44:37 +0300

    Add emotion description in ChartItemViewController

--- Sources/EmotionCode/ChartItemViewController.swift
+++ Sources/EmotionCode/ChartItemViewController.swift
@@ -4,4 +4,18 @@ final class ChartItemViewController: UICollectionViewController {
 
     var item: Chart.Item!
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let indexPath = collectionView?.indexPathsForSelectedItems?.first,
+            let cell = collectionView?.cellForItem(at: indexPath) as? ItemCollectionViewCell else { return }
+        cell.configure(title: item.description)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        guard let indexPath = collectionView?.indexPathsForSelectedItems?.first,
+            let cell = collectionView?.cellForItem(at: indexPath) as? ItemCollectionViewCell else { return }
+        cell.configure(title: item.title)
+    }
+
 }
