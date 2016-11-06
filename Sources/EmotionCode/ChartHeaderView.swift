b30aac5704f36641a27ac8ab5e73fae91f4a58a7
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-11-06 15:54:52 +0300

    Configure ChartHeaderView

--- Sources/EmotionCode/ChartHeaderView.swift
+++ Sources/EmotionCode/ChartHeaderView.swift
@@ -2,4 +2,18 @@ import UIKit
 
 final class ChartHeaderView: UICollectionReusableView {
 
+    private let titleLabel = UILabel()
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        titleLabel.frame = bounds
+        titleLabel.textAlignment = .center
+        titleLabel.font = .preferredFont(forTextStyle: .footnote)
+        addSubview(titleLabel)
+    }
+
+    func configure(title: String) {
+        titleLabel.text = title
+    }
+
 }
