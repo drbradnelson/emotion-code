8ea931aaf4350b052b58c821182b389d6ea3b470
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-12-08 22:02:16 +0300

    Move chart item background colors to storyboard

--- Sources/EmotionCode/ItemCollectionViewCell.swift
+++ Sources/EmotionCode/ItemCollectionViewCell.swift
@@ -5,10 +5,10 @@ final class ItemCollectionViewCell: UICollectionViewCell {
     @IBOutlet private var titleLabel: UILabel!
     @IBOutlet private var descriptionTextView: UITextView!
 
-    static let preferredReuseIdentifier = "ItemCell"
+    @IBInspectable private var oddRowColor: UIColor!
+    @IBInspectable private var evenRowColor: UIColor!
 
-    private static let oddRowColor = UIColor(red: 0.56, green: 0.78, blue: 0.78, alpha: 1)
-    private static let evenRowColor = UIColor(red: 0.66, green: 0.88, blue: 0.98, alpha: 1)
+    static let preferredReuseIdentifier = "ItemCell"
 
     func configure(with emotion: Chart.Emotion) {
         titleLabel.text = emotion.title
@@ -18,7 +18,7 @@ final class ItemCollectionViewCell: UICollectionViewCell {
     func setBackgroundColor(for indexPath: IndexPath) {
         let row = (indexPath.section + ChartLayout.numberOfColumns) / ChartLayout.numberOfColumns
         let isRowEven = (row % ChartLayout.numberOfColumns == 0)
-        backgroundColor = isRowEven ? ItemCollectionViewCell.evenRowColor : ItemCollectionViewCell.oddRowColor
+        backgroundColor = isRowEven ? evenRowColor : oddRowColor
     }
 
     func setDescriptionVisible(_ visible: Bool) {
