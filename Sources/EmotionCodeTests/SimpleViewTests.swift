aec146a2682fe39bbe014a3f3dae2c568072e9a2
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-12-19 19:27:13 +0300

    Add SimpleViewTests, remove ChartLayoutModuleViewTests

---
+++ Sources/EmotionCodeTests/SimpleViewTests.swift
@@ -0,0 +1,39 @@
+import XCTest
+@testable import EmotionCode
+
+final class SimpleViewTests: XCTestCase {
+        
+    func testItemFramesCount() {
+        var model = Model()
+        let itemsPerSection = [0, 1, 2, 3 ,4]
+        model.itemsPerSection = itemsPerSection
+        
+        let view = Module.view(for: model)
+
+        XCTAssertEqual(view.itemFrames.count, itemsPerSection.count)
+    }
+
+    func testColumnHeaderFramesCount() {
+        var model = Model()
+        model.itemsPerSection = [0, 1, 2, 3, 4]
+
+        let view = Module.view(for: model)
+
+        XCTAssertEqual(view.columnHeaderFrames.count, View.numberOfColumns)
+    }
+
+    func testRowHeaderFramesCount() {
+        var model = Model()
+
+        model.itemsPerSection = [
+            0, 1,
+            2, 3,
+            4
+        ]
+
+        let view = Module.view(for: model)
+
+        XCTAssertEqual(view.rowHeaderFrames.count, 3)
+    }
+    
+}
