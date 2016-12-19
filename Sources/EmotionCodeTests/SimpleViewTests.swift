fa193a6c3635ef54231a7eb0ef1be26e4cfbd605
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-12-19 19:31:22 +0300

    Improve tests in SimpleViewTests

--- Sources/EmotionCodeTests/SimpleViewTests.swift
+++ Sources/EmotionCodeTests/SimpleViewTests.swift
@@ -13,9 +13,27 @@ final class SimpleViewTests: XCTestCase {
         XCTAssertEqual(view.itemFrames.count, itemsPerSection.count)
     }
 
+    func testItemFramesPerSectionCount() {
+        var model = Model()
+        let itemsPerSection = [0, 1, 2, 3, 4]
+        model.itemsPerSection = itemsPerSection
+
+        let view = Module.view(for: model)
+
+        XCTAssertEqual(view.itemFrames[0].count, itemsPerSection[0])
+        XCTAssertEqual(view.itemFrames[1].count, itemsPerSection[1])
+        XCTAssertEqual(view.itemFrames[2].count, itemsPerSection[2])
+        XCTAssertEqual(view.itemFrames[3].count, itemsPerSection[3])
+        XCTAssertEqual(view.itemFrames[4].count, itemsPerSection[4])
+    }
+
     func testColumnHeaderFramesCount() {
         var model = Model()
-        model.itemsPerSection = [0, 1, 2, 3, 4]
+        model.itemsPerSection = [
+            0, 1,
+            2, 3,
+            4
+        ]
 
         let view = Module.view(for: model)
 
