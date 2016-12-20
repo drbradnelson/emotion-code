c99c651889b72e2e9135045cc7633a99059e68af
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-12-20 20:33:10 +0300

    Use literals in ViewChartSizeTests

--- Sources/EmotionCodeTests/ViewChartSizeTests.swift
+++ Sources/EmotionCodeTests/ViewChartSizeTests.swift
@@ -5,30 +5,35 @@ final class ViewChartSizeTests: XCTestCase {
 
     func testModeAll() {
         var model = Model()
-        model.itemsPerSection = [1, 2, 3, 4, 5]
-        let viewSize = Size(width: 123, height: 456)
+        model.itemsPerSection = [
+            1, 2,
+            3, 4,
+            5
+        ]
+        let viewSize = Size(width: 100, height: 200)
         model.viewSize = viewSize
 
         let view = Module.view(for: model)
 
-        let width = viewSize.width
-        let height = view.rowHeaderFrames.last!.maxY + model.contentPadding
-        let expectedSize = Size(width: width, height: height)
+        let expectedSize = Size(width: 100, height: 605)
         XCTAssertEqual(view.chartSize, expectedSize)
     }
 
     func testModeSection() {
         var model = Model()
         model.mode = .section(0)
-        model.itemsPerSection = [1, 2, 3, 4, 5]
-        let viewSize = Size(width: 123, height: 456)
+        model.itemsPerSection = [
+            1, 2,
+            3, 4,
+            5
+        ]
+        let viewSize = Size(width: 100, height: 100)
         model.viewSize = viewSize
 
         let view = Module.view(for: model)
 
-        let width = view.columnHeaderFrames.last!.maxX + model.contentPadding
-        let height = view.rowHeaderFrames.last!.maxY + model.contentPadding
-        let expectedSize = Size(width: width, height: height)
+
+        let expectedSize = Size(width: 210, height: 310)
         XCTAssertEqual(view.chartSize, expectedSize)
     }
 
@@ -36,15 +41,17 @@ final class ViewChartSizeTests: XCTestCase {
         var model = Model()
         let indexPath = IndexPath(item: 0, section: 1)
         model.mode = .emotion(indexPath)
-        model.itemsPerSection = [1, 2, 3, 4, 5]
-        let viewSize = Size(width: 123, height: 456)
+        model.itemsPerSection = [
+            1, 2,
+            3, 4,
+            5
+        ]
+        let viewSize = Size(width: 100, height: 100)
         model.viewSize = viewSize
 
         let view = Module.view(for: model)
 
-        let width = view.columnHeaderFrames.last!.maxX + model.contentPadding
-        let height = view.rowHeaderFrames.last!.maxY + model.contentPadding
-        let expectedSize = Size(width: width, height: height)
+        let expectedSize = Size(width: 210, height: 1270)
         XCTAssertEqual(view.chartSize, expectedSize)
     }
 
