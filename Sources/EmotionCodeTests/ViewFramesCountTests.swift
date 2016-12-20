ebac22fe5173c11b604c9b8a5702026176f2305f
Maxim Nasakin <nasakinmaxim@gmail.com>
2016-12-20 20:42:24 +0300

    Use literals in ViewFramesCountTests

--- Sources/EmotionCodeTests/ViewFramesCountTests.swift
+++ Sources/EmotionCodeTests/ViewFramesCountTests.swift
@@ -5,26 +5,32 @@ final class ViewFramesCountTests: XCTestCase {
 
     func testItemFrames() {
         var model = Model()
-        let itemsPerSection = [0, 1, 2, 3 ,4]
-        model.itemsPerSection = itemsPerSection
-        
+        model.itemsPerSection = [
+            0, 1,
+            2, 3,
+            4
+        ]
+
         let view = Module.view(for: model)
 
-        XCTAssertEqual(view.itemFrames.count, itemsPerSection.count)
+        XCTAssertEqual(view.itemFrames.count, 5)
     }
 
     func testItemFramesPerSection() {
         var model = Model()
-        let itemsPerSection = [0, 1, 2, 3, 4]
-        model.itemsPerSection = itemsPerSection
+        model.itemsPerSection = [
+            0, 1,
+            2, 3,
+            4
+        ]
 
         let view = Module.view(for: model)
 
-        XCTAssertEqual(view.itemFrames[0].count, itemsPerSection[0])
-        XCTAssertEqual(view.itemFrames[1].count, itemsPerSection[1])
-        XCTAssertEqual(view.itemFrames[2].count, itemsPerSection[2])
-        XCTAssertEqual(view.itemFrames[3].count, itemsPerSection[3])
-        XCTAssertEqual(view.itemFrames[4].count, itemsPerSection[4])
+        XCTAssertEqual(view.itemFrames[0].count, 0)
+        XCTAssertEqual(view.itemFrames[1].count, 1)
+        XCTAssertEqual(view.itemFrames[2].count, 2)
+        XCTAssertEqual(view.itemFrames[3].count, 3)
+        XCTAssertEqual(view.itemFrames[4].count, 4)
     }
 
     func testColumnHeaderFrames() {
@@ -37,12 +43,11 @@ final class ViewFramesCountTests: XCTestCase {
 
         let view = Module.view(for: model)
 
-        XCTAssertEqual(view.columnHeaderFrames.count, View.numberOfColumns)
+        XCTAssertEqual(view.columnHeaderFrames.count, 2)
     }
 
     func testRowHeaderFrames() {
         var model = Model()
-
         model.itemsPerSection = [
             0, 1,
             2, 3,
