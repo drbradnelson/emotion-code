c8c4be463193fe27e353b1228f8c985cc46600c8
Rudolf Adamkovič <salutis@me.com>
2017-01-04 14:59:35 +0100

    Update to Elm 0.8.x

--- Sources/EmotionCodeTests/Utilities.swift
+++
@@ -1,9 +0,0 @@
-protocol StringEquatable: Equatable {
-    static func == (lhs: Self, rhs: Self) -> Bool
-}
-
-extension StringEquatable {
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        return String(describing: lhs) == String(describing: rhs)
-    }
-}
