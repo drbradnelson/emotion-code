0fbc7c1a634e08aab9cf7be51a0a9d9838bfa79b
Rudolf Adamkovič <salutis@me.com>
2017-01-19 12:55:09 +0100

    Simplify ConvertBook script (close #177)

--- Sources/EmotionCode/ConvertBook.sh
+++ Sources/EmotionCode/ConvertBook.sh
@@ -1,38 +1,19 @@
 #!/usr/bin/env bash -e
 
-UNLOCALIZED_RESOURCES_FOLDER_PATH=${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}
-ENGLISH_LOCALIZATION_FOLDER_PATH=$UNLOCALIZED_RESOURCES_FOLDER_PATH/en.lproj
-SPANISH_LOCALIZATION_FOLDER_PATH=$UNLOCALIZED_RESOURCES_FOLDER_PATH/es.lproj
+RESOURCES="../Resources"
 
-mkdir -p $ENGLISH_LOCALIZATION_FOLDER_PATH
-mkdir -p $SPANISH_LOCALIZATION_FOLDER_PATH
+function install {
+    SOURCE="$1"
+    DESTINATION="$2"
+    mkdir -p "$DESTINATION"
+    cp -r "$RESOURCES"/CSS "$RESOURCES"/English/Images "$DESTINATION"
+    find "$SOURCE"/* -iname "*.md" -type f | while read INPUT; do
+        OUTPUT="$DESTINATION"/`basename "$INPUT" .md`.html
+        pandoc "$INPUT" --from=markdown --to=html5 --standalone --output="$OUTPUT" --css=main.css
+    done
+}
 
-#English version
-cp -r ../Resources/CSS/. $ENGLISH_LOCALIZATION_FOLDER_PATH/
-cp -r ../Resources/English/Images/. $ENGLISH_LOCALIZATION_FOLDER_PATH/
-pandoc ../Resources/English/Markdown/chapter1.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter1.html --css=main.css
-pandoc ../Resources/English/Markdown/chapter2.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter2.html --css=main.css
-pandoc ../Resources/English/Markdown/chapter3.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter3.html --css=main.css
-pandoc ../Resources/English/Markdown/chapter4.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter4.html --css=main.css
-pandoc ../Resources/English/Markdown/chapter5.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter5.html --css=main.css
-pandoc ../Resources/English/Markdown/chapter6.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter6.html --css=main.css
-pandoc ../Resources/English/Markdown/chapter7.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter7.html --css=main.css
-pandoc ../Resources/English/Markdown/chapter8.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter8.html --css=main.css
-pandoc ../Resources/English/Markdown/chapter9.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter9.html --css=main.css
-pandoc ../Resources/English/Markdown/chapter10.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/chapter10.html --css=main.css
-pandoc ../Resources/English/Markdown/about-author.md --from=markdown --to=html5 --standalone --output=$ENGLISH_LOCALIZATION_FOLDER_PATH/about-author.html --css=main.css
+BUNDLE="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH"
 
-#Spanish version
-cp -r ../Resources/CSS/. $SPANISH_LOCALIZATION_FOLDER_PATH/
-cp -r ../Resources/Spanish/Images/. $SPANISH_LOCALIZATION_FOLDER_PATH/
-pandoc ../Resources/Spanish/Markdown/chapter1.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter1.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/chapter2.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter2.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/chapter3.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter3.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/chapter4.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter4.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/chapter5.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter5.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/chapter6.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter6.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/chapter7.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter7.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/chapter8.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter8.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/chapter9.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter9.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/chapter10.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/chapter10.html --css=main.css
-pandoc ../Resources/Spanish/Markdown/about-author.md --from=markdown --to=html5 --standalone --output=$SPANISH_LOCALIZATION_FOLDER_PATH/about-author.html --css=main.css
+install "$RESOURCES"/English/Markdown "$BUNDLE"/en.lproj
+install "$RESOURCES"/Spanish/Markdown "$BUNDLE"/es.lproj
