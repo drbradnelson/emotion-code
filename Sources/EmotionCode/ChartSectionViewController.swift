ed5fd4861a30122b836ac65382fae45e9ef2676c
Maxim Nasakin <nasakinmaxim@gmail.com>
2017-03-14 19:14:19 +0300

    Move layout methods to UICollectionViewController extension

--- Sources/EmotionCode/ChartSectionViewController.swift
+++ Sources/EmotionCode/ChartSectionViewController.swift
@@ -59,21 +59,6 @@ final class ChartSectionViewController: UICollectionViewController {
         destination.set(emotion)
     }
 
-    // MARK: Layout
-
-    private func layoutCellsAlongsideTransition() {
-        transitionCoordinator?.animate(alongsideTransition: { [collectionView] _ in
-            collectionView!.visibleCells.forEach { $0.layoutIfNeeded() }
-        }, completion: nil)
-    }
-
-    private func layoutSupplementaryViewsAlongsideTransition(withKinds kinds: [String]) {
-        transitionCoordinator?.animate(alongsideTransition: { [collectionView] _ in
-            let supplementaryViews = kinds.flatMap(collectionView!.visibleSupplementaryViews)
-            supplementaryViews.forEach { $0.layoutIfNeeded() }
-        }, completion: nil)
-    }
-
 }
 
 extension ChartSectionViewController: ChartPresenter {
