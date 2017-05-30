8f0e68fd2785b0433d3e3da6c6bdbbbb7702dacb
Maxim Nasakin <nasakinmaxim@gmail.com>
2017-05-30 17:18:47 +0300

    Add ChartLayoutCalculator

--- Sources/EmotionCode/ChartLayoutCalculator.swift
+++ Sources/EmotionCode/ChartLayoutCalculator.swift
@@ -1,9 +1,61 @@
-//
-//  ChartLayoutCalculator.swift
-//  EmotionCode
-//
-//  Created by Maxim Nasakin on 30/05/2017.
-//  Copyright © 2017 DiscoverHealing.com. All rights reserved.
-//
+protocol ChartLayoutCalculatorInterface: class {
 
-import Foundation
+    associatedtype Item
+    associatedtype Header
+
+    var chartSize: Size { get }
+    var items: [IndexPath: Item] { get }
+    var columnHeaders: [IndexPath: Header] { get }
+    var rowHeaders: [IndexPath: Header] { get }
+    var labelSizes: [IndexPath: Size] { get }
+
+}
+
+final class ChartLayoutCalculator: ChartLayoutCalculatorInterface {
+
+    enum Mode {
+        case all
+        case section(Int, isFocused: Bool)
+        case emotion(IndexPath, isFocused: Bool)
+    }
+
+    struct Item {
+        let frame: Rect
+        let alpha: Float
+    }
+    typealias Header = Item
+
+    private struct Parameters {
+        static let minViewHeightForCompactLayout = 554
+        static let headerSize = Size(width: 30, height: 30)
+        static let itemHeight = 18
+        static let itemPadding = 8
+        static let contentPadding = 10
+        static let sectionSpacing = Size(width: 5, height: 5)
+        static let itemSpacing = 10
+    }
+
+    var chartSize: Size = .zero
+    var items: [IndexPath: Item] = [:]
+    var columnHeaders: [IndexPath: Header] = [:]
+    var rowHeaders: [IndexPath: Header] = [:]
+    var labelSizes: [IndexPath: Size] = [:]
+
+    private var mode: Mode
+    private let numberOfColumns: Int
+    private let itemsPerSection: [Int]
+
+    private var viewSize: Size
+    private let topContentInset: Int
+    private let bottomContentInset: Int
+
+    init(mode: Mode, numberOfColumns: Int, itemsPerSection: [Int], viewSize: Size, topContentInset: Int, bottomContentInset: Int) {
+        self.mode = mode
+        self.numberOfColumns = numberOfColumns
+        self.itemsPerSection = itemsPerSection
+        self.viewSize = viewSize
+        self.topContentInset = topContentInset
+        self.bottomContentInset = bottomContentInset
+    }
+
+}
