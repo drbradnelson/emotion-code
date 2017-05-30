b95b346a2a1c600af7a888f029cb7918ede1aa92
Maxim Nasakin <nasakinmaxim@gmail.com>
2017-05-30 17:17:23 +0300

    Add Geometry file with types for ChartLayout calculators

---
+++ Sources/EmotionCode/ChartLayoutCalculator.swift
@@ -0,0 +1,9 @@
+//
+//  ChartLayoutCalculator.swift
+//  EmotionCode
+//
+//  Created by Maxim Nasakin on 30/05/2017.
+//  Copyright © 2017 DiscoverHealing.com. All rights reserved.
+//
+
+import Foundation
